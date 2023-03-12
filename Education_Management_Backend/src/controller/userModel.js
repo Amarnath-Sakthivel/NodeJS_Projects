1cdddc8659ff3a7bf9961edacc883933e0367567
Amarnath Sakthivel <63423850+Amarnath-Sakthivel@users.noreply.github.com>
2023-03-12 19:52:10 +0530

    chore: added signin endpoint

--- Education_Management_Backend/src/controller/userModel.js
+++ Education_Management_Backend/src/controller/userModel.js
@@ -42,16 +42,59 @@ const fetchAllUser = async (req, res, next) => {
  *
  */
 const fetchUserByID = (req, res, next) => {
-  dbConnection().connect(function (err) {
-    if (err) next(err);
-    dbConnection().query(
-      `SELECT * FROM student WHERE username = '${req.query.email}'`,
-      function (err, result) {
-        if (err) next(err);
-        res.json({ user: result });
-      }
-    );
-  });
+  if (!req.query.email) {
+    var err = new Error();
+    err.status = 400;
+    err.message = `query params 'email' missing in request`;
+    next(err);
+  } else {
+    dbConnection().connect(function (err) {
+      if (err) next(err);
+      dbConnection().query(
+        `SELECT * FROM student WHERE username = '${req.query.email}'`,
+        function (err, result) {
+          if (err) next(err);
+          res.json({ user: result });
+        }
+      );
+    });
+  }
 };
 
-module.exports = { createUser, fetchAllUser, fetchUserByID };
+
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ * 
+ * @return True if user exist in database
+ */
+
+const isValidUser = (req, res, next) => {
+  var error = new Error();
+  error.status = 400;
+  if (!req.headers.username || !req.headers.password) {
+    error.message = `username or password is missing in request header`;
+    next(error);
+  } else {
+    dbConnection().connect(function (err) {
+      if (err) next(err);
+      dbConnection().query(
+        `SELECT * FROM student WHERE username = '${req.headers.username}' AND password='${req.headers.password}'`,
+        function (err, result) {
+          if (err) next(err);
+          else if (result.length == 0) {
+            error.status = 401;
+            error.message = "invalid user";
+            next(error)
+          } else {
+            res.json({ message: true });
+          }
+        }
+      );
+    });
+  }
+};
+
+module.exports = { createUser, fetchAllUser, fetchUserByID, isValidUser };
--- Education_Management_Backend/src/router/api.js
+++ Education_Management_Backend/src/router/api.js
@@ -1,5 +1,5 @@
 const authorizeUser = require('../controller/auth');
-const { createUser, fetchAllUser, fetchUserByID } = require('../controller/userModel');
+const { createUser, fetchAllUser, fetchUserByID, isValidUser } = require('../controller/userModel');
 const { errorHandlingMiddleware } = require('../middleware/errorHandler');
 
 const router = require('express').Router();
@@ -12,6 +12,7 @@ const router = require('express').Router();
 //routes
 router.use(authorizeUser)
 router.post('/signup', createUser)
+router.get('/signin', isValidUser)
 router.get('/users',fetchAllUser)
 router.get('/user', fetchUserByID);
 router.use(errorHandlingMiddleware)
